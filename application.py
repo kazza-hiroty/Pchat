4bcbd6e43f9bbab5da4f58c56e6ed51c85c33c9d
Kazuki Hirota <k.hirota0129@gmail.com>
2020-06-23 16:46:29 +1000

    third

--- application.py
+++ application.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 
+from wtform_fields import *
+
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Greate sucess!"
 
-    return render_template("index.html")
+    return render_template("index.html", form = reg_form)
 
 if __name__ == "__main__":
     
