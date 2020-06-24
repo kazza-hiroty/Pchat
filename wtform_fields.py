37f3ba241d7fe63547692f4df97ba0f95b3f614d
Kazuki Hirota <k.hirota0129@gmail.com>
2020-06-24 10:32:54 +1000

    Create login page and login validation

--- wtform_fields.py
+++ wtform_fields.py
@@ -1,6 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+
+from models import User
+
+#check if the password mathes user
+#you can reuse it if you write this function ouside of loginform
+def invalid_credentials(form,field):
+    username_entered = form.username.data
+    password_entered = field.data
+
+    user_object = User.query.filter_by(username=username_entered).first()
+    if user_object is None:
+        raise ValidationError("Username or password is incorrect")
+    elif password_entered != user_object.password:
+        raise ValidationError("Username or password is incorrect")
 
 class RegistrationForm(FlaskForm):
     
@@ -8,3 +22,13 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('password_label', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
     confirm_pswd = PasswordField('confirm_pswd_label', validators=[InputRequired(message="Username required"), EqualTo('password', message="Passwords must match")])
     submit_button = SubmitField('Create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("User name already exist. select different username")
+
+class LoginForm(FlaskForm):
+    username = StringField('username_label', validators=[InputRequired(message="Username required")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Password required"), invalid_credentials])
+    submit_button = SubmitField('Login')
